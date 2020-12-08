448d8c48d7eeeb83c2b132dc607d9bb43dae70c6
Oluwaseun <ekunkoya.seun@gmail.com>
2020-12-08 14:00:55 +0100

    first commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export const AddTransaction = () => {
+  //text is the varible name of what we need to get from the form
+  //setText is a function to manipulate he state
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("");
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form>
+        <div className='form-control'>
+          <label htmlFor='text'>Text</label>
+          <input
+            type='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder='Enter text...'
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='amount'>
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type='number'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder='Enter amount...'
+          />
+        </div>
+        <button className='btn'>Add transaction</button>
+      </form>
+    </>
+  );
+};
