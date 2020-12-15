f96376a3e145222174dda304461d32e4d6855fd8
Oluwaseun <ekunkoya.seun@gmail.com>
2020-12-15 07:58:31 +0100

    more commits

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,7 +1,20 @@
 
 // @desc Get all transactions
-// @route  /api/v1/transactions
+// @route  GET /api/v1/transactions
 // @access Public
 exports.getTransactions = (req, res, next) => {
     res.send('Get Transactions')
+}
+
+// @desc Add transaction
+// @route  POST /api/v1/transactions
+// @access Public
+exports.addTransactions = (req, res, next) => {
+    res.send('POST Transactions')
+}
+// @desc Delete all transactions
+// @route  DELETE /api/v1/transactions/:id
+// @access Public
+exports.deleteTransactions = (req, res, next) => {
+    res.send('Delete Transactions')
 }
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {getTransactions} = require('../controllers/transactions')
+const {
+  getTransactions,
+  addTransactions,
+  deleteTransactions,
+} = require("../controllers/transactions");
 
-router.get("/", (req, res) => res.send("Hello"));
+router.route("/").get(getTransactions).post(addTransactions);
+
+router.route("/:id").delete(deleteTransactions);
 
 module.exports = router;
